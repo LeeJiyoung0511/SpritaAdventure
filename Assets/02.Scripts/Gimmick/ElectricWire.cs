be7c206410d6e263f774687be1e310edeebeec3d
LeeJiyoung <cjstk7597@gmail.com>
2025-03-11 13:33:46 +0900

    feat : 전깃줄 씬 구현
    전깃줄 주석 코드, 전깃줄 데미지 처리 추가

--- Assets/02.Scripts/Gimmick/ElectricWire.cs
+++ Assets/02.Scripts/Gimmick/ElectricWire.cs
@@ -9,24 +9,27 @@ public class ElectricWire : MonoBehaviour
     [SerializeField]
     private LayerMask m_PlayerLayer; //플레이어 레이어
 
+    public float Damage = 10.0f;
+
     private void FixedUpdate()
     {
-        Check();
+        CheckHitPlayer();
     }
 
-    private void Check()
+    //전깃줄 레이 충돌 처리
+    private void CheckHitPlayer()
     {
-        Vector3 startPos = m_StartPos.position;
-        Vector3 endPos = m_EndPos.position;
-        Vector3 direction = (endPos - startPos).normalized;
-        float distance = Vector3.Distance(startPos, endPos);
+        Vector3 startPos = m_StartPos.position;  // 전깃줄 시작위치
+        Vector3 endPos = m_EndPos.position; //전깃줄 끝 위치
+        Vector3 direction = (endPos - startPos).normalized; //전깃줄 방향
+        float distance = Vector3.Distance(startPos, endPos); //시작지점과 끝지점의 전깃줄 거리
         Ray ray = new Ray(startPos, direction);
 
         if (Physics.Raycast(ray, out RaycastHit hit, distance, m_PlayerLayer))
         {
-            if (hit.collider.TryGetComponent(out PlayerController controller))
+            if (hit.collider.TryGetComponent(out Player player))
             {
-                controller.Knockback();
+                player.Damage(Damage);  // 전깃줄에 닿으면 플레이어 넉백
             }
         }
     }
